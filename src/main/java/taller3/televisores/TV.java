4a0c836894e4aa408d063df0f4b631dc18b125cf
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-08-25 14:00:26 -0500

    C

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -3,16 +3,19 @@ package taller3.televisores;
 public class TV {
 	
 	private Marca marca;
-	private int canal = 1;
-	private int precio = 500;
+	private int canal;
+	private int precio;
 	private boolean estado;
-	private int volumen = 1;
+	private int volumen ;
 	private Control control;
 	private static int numTV = 0;
 	
 	public TV(Marca marca, boolean estado) {
 		this.marca = marca;
 		this.estado = estado;
+		this.canal = 1;
+		this.precio = 500;
+		this.volumen = 1;
 		numTV++ ;	
 	}
 	
