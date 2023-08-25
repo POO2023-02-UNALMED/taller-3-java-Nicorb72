b2027916bcb6f97e56c28d88eb3d695bd64b598b
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-08-25 09:24:11 -0500

    Tv

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,95 @@
+package taller3.televisores;
+
+public class TV {
+	
+	private Marca marca;
+	private int canal = 1;
+	private int precio = 500;
+	private boolean estado;
+	private int volumen = 1;
+	private Control control;
+	private static int numTV = 0;
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca = marca;
+		this.estado = estado;
+		numTV++ ;	
+	}
+	
+	public Marca getMarca() {
+		return this.marca;
+	}
+	
+	public void setMarca (Marca marca) {
+		this.marca = marca;
+	}
+	public int getCanal() {
+		return this.canal;
+	}
+	
+	public void setCanal (int canal) {
+		this.canal = canal;
+	}
+	public int getPrecio() {
+		return this.precio;
+	}
+	
+	public void setPrecio (int precio) {
+		this.precio = precio;
+	}
+	public int getvolumen() {
+		return this.volumen;
+	}
+	
+	public void setVolumen (int volumen) {
+		this.volumen = volumen;
+	}
+	public Control getControl() {
+		return this.control;
+	}
+	
+	public void setControl (Control control) {
+		this.control = control;
+	}
+
+	public static int getNumTV() {
+		return numTV;
+	}
+
+	public static void setNumTV(int numTV) {
+		TV.numTV = numTV;
+	}
+	public void turnOn() {
+		if (this.estado != true);{
+			this.estado = true;
+		}	
+	}
+	public void turnOff() {
+		if (this.estado != false);{
+			this.estado = false;
+		}	
+	}
+	public void canalUp () {
+		if (this.estado == true && this.canal < 120); {
+			this.canal++ ;
+		}
+	}
+	public void canalDown () {
+		if (this.estado == true && this.canal > 1); {
+			this.canal-- ;
+		}
+	}
+	public void VolumenlUp () {
+		if (this.estado == true && this.volumen < 7); {
+			this.volumen++ ;
+		}
+	}
+	public void VolumenDown () {
+		if (this.estado == true && this.volumen > 0); {
+			this.volumen-- ;
+		}
+	}
+	
+			
+}
+
