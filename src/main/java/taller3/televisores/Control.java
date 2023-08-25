26cc7627dc52ff2ef3d428aeb7f7e4fb7aecbf74
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-08-25 10:26:42 -0500

    2

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -14,28 +14,28 @@ public class Control {
 		tv.setControl(this);
 	}
 	public void turnOn () {
-		this.tv.turnOn();
+		tv.turnOn();
 	}
 	public void turnOff () {
-		this.tv.turnOff();
+		tv.turnOff();
 	}
 	public void canalUp() {
-		this.tv.canalUp();
+		tv.canalUp();
 	}
 	public void canalDown() {
-		this.tv.canalDown();
+		tv.canalDown();
 	}
 	public void volumenUp() {
-		this.tv.volumenUp();
+		tv.volumenUp();
 	}
 	public void volumenDown() {
-		this.tv.volumenDown();
+		tv.volumenDown();
 	}
 	public void setVolumen (int volumen) {
-		this.tv.setVolumen(volumen);
+		tv.setVolumen(volumen);
 	}
 	public void setCanal (int canal) {
-		this.tv.setCanal(canal);
+		tv.setCanal(canal);
 	}
 
 
